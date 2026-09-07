a66fa2b1703b0510fff97acc2f80a7e922117760
NadeenElshenawy <nadeensamy415@gmail.com>
2026-09-08 00:38:44 +0300

    moved the education data to another separate js file

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,37 +1,9 @@
 import "./Education.css";
 import { ScrollReveal } from "./ScrollReveal";
+import { timelineData } from "../data/timeLineData";
 
 export const Education = () => {
-  const timelineData = [
-    {
-      id: 1,
-      period: "2024 – 2028",
-      degree: "B.Sc. Information Technology",
-      institution: "Borg El-Arab Technological University",
-      description:
-        "Currently enrolled in the Software Department, studying software development, web technologies, databases, and computer science fundamentals.",
-      isCurrent: true,
-    },
-    {
-      id: 2,
-      period: "2026 — Present",
-      degree: "DEPI React Web Development Track",
-      institution: "Digital Egypt Pioneers Initiative (DEPI)",
-      description:
-        "Currently enrolled in a structured React Web Development track, building my understanding of React, component-based development, and modern front-end development through hands-on learning.",
-      isCurrent: true,
-    },
-    {
-      id: 3,
-      period: "2024 — Present",
-      degree: "Front-End Development",
-      institution: "Self-directed learning + hands-on projects",
-      description:
-        "Developing my front-end skills through practical projects using HTML, CSS, JavaScript, React, and Tailwind CSS. I focus on turning ideas and designs into responsive, user-friendly interfaces while continuously improving through each project.",
-      isCurrent: false,
-    },
-  ];
-
+  
   return (
     <section className="education-section" id="education">
       <div className="education-container">
